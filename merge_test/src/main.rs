bf63eb6b6e3e38202906ce0941a318d5ec974d1f
steensgaard <34754189+steensgaard@users.noreply.github.com>
2018-11-25 12:46:15 +0100

    added code to mearge sort and added some tests

--- merge_test/src/main.rs
+++ merge_test/src/main.rs
@@ -66,8 +66,7 @@ mod mearge {
         assert_eq!(output, mearge_sort(input));
     }
 
-
-        #[test]
+    #[test]
     fn mearge_sort_can_handle_negative_numbers() {
         let mut input: Vec<i32> = Vec::new();
         input.push(10);
@@ -93,46 +92,96 @@ mod mearge {
         output.push(10);
         assert_eq!(output, mearge_sort(input));
     }
-}
 
-fn mearge_sort(list: Vec<i32>) -> Vec<i32> {
-    if list.len() == 0 || list.len() == 1 {
-        return list.clone();
-    } else {
-        let mut result: Vec<i32> = Vec::new();
+    #[test]
+    fn mearge_sort_long_sorted_list() {
+        let mut input: Vec<i32> = Vec::new();
+        input.push(-6);
+        input.push(-3);
+        input.push(1);
+        input.push(2);
+        input.push(4);
+        input.push(5);
+        input.push(7);
+        input.push(8);
+        input.push(9);
+        input.push(10);
+        let mut output: Vec<i32> = Vec::new();
+        output.push(-6);
+        output.push(-3);
+        output.push(1);
+        output.push(2);
+        output.push(4);
+        output.push(5);
+        output.push(7);
+        output.push(8);
+        output.push(9);
+        output.push(10);
+        assert_eq!(output, mearge_sort(input));
+    }
 
-        let left = &mut mearge_sort(list[0..list.len() / 2].to_vec());
-        let right = &mut mearge_sort(list[list.len() / 2..list.len()].to_vec());
-
-        let mut left_index = 0;
-        let mut right_index = 0;
-        if left.len() == 0 {
-            return right.clone();
-        }
-        if right.len() == 0 {
-            return left.clone();
-        }
-
-        while left_index < left.len() && right_index < right.len() {
-            if left[left_index] < right[right_index] {
-                result.push(left[left_index]);
-                left_index += 1;
-            } else {
-                result.push(right[right_index]);
-                right_index += 1;
-            }
-            if left_index >= left.len() {
-                result.append(&mut right[right_index..].to_vec())
-            }
-            if right_index >= right.len() {
-                result.append(&mut left[left_index..].to_vec())
-            }
-        }
-
-        return result;
+    #[test]
+    fn mearge_sort_long_reversed_sorted_list() {
+        let mut input: Vec<i32> = Vec::new();
+        input.push(10);
+        input.push(9);
+        input.push(8);
+        input.push(7);
+        input.push(5);
+        input.push(4);
+        input.push(2);
+        input.push(1);
+        input.push(-3);
+        input.push(-6);
+        let mut output: Vec<i32> = Vec::new();
+        output.push(-6);
+        output.push(-3);
+        output.push(1);
+        output.push(2);
+        output.push(4);
+        output.push(5);
+        output.push(7);
+        output.push(8);
+        output.push(9);
+        output.push(10);
+        assert_eq!(output, mearge_sort(input));
     }
 }
 
+fn mearge_sort(list: Vec<i32>) -> Vec<i32> {
+    if list.len() < 2 {
+        return list.clone();
+    } else {
+        let left = mearge_sort(list[0..list.len() / 2].to_vec());
+        let right = mearge_sort(list[list.len() / 2..list.len()].to_vec());
+        return mearge(left, right);
+    }
+}
+
+fn mearge(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    let mut left_index = 0;
+    let mut right_index = 0;
+    while left_index < left.len() && right_index < right.len() {
+        if left[left_index] < right[right_index] {
+            result.push(left[left_index]);
+            left_index += 1;
+        } else {
+            result.push(right[right_index]);
+            right_index += 1;
+        }
+        if left_index >= left.len() {
+            let mut rest = right[right_index..].to_vec();
+            result.append(&mut rest)
+        }
+        if right_index >= right.len() {
+            let mut rest = left[left_index..].to_vec();
+            result.append(&mut rest)
+        }
+    }
+    return result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!!!");
 }
